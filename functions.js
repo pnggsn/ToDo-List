d6cd30d9ab8deae903ac1124949bce2523dacf1d
Mosunova <81022626+pnggsn@users.noreply.github.com>
2022-04-17 01:28:48 +0700

    ver 0.95 pre-finished

--- functions.js
+++ functions.js
@@ -32,121 +32,64 @@ function Add() {
         li.appendChild(btn);
         ul.appendChild(li);
         document.getElementById("input").value = '';
+        display(doneTasks, notDoneTasks, true);
 
-        btn.addEventListener('click', (e) => {
+        btn.addEventListener('click', () => {
             ul.removeChild(li);
         });
 
-        li.addEventListener('click', (e) => {
-            if (!check.checked) {
-                check.checked = true;
-                li.id = "liChecked";
-                check.className="_done"
-            } else {
-                check.checked = false;
-                li.id = null;
-                check.className="_notDone"
-            }
-        });
-        check.addEventListener('click', (e) => {
-            if (!check.checked) {
-                check.checked = true;
-                li.id = "liChecked";
-                check.className="_done"
-            } else {
-                check.checked = false;
-                li.id = null;
-                check.className="_notDone"
-            }
-        });
+        li.addEventListener('click', makeChecked);
+        check.addEventListener('click', makeChecked);
 
-        // check_notDone.addEventListener("change", (e) => {
-        //     if (check_notDone.checked) {
-        //         check_done.checked = false;
-        //         check_all.checked = false;
-        //
-        //         // for(let i=0; ll=lis[i]; i++) {
-        //         //     if (ll[0].checked){
-        //         //         ll.style.visibility = 'hidden';
-        //         //         ll.style.position = 'absolute';
-        //         //     }
-        //         //     else {
-        //         //         ll.style.visibility = 'visible';
-        //         //         ll.style.position = 'relative';
-        //         //     }
-        //         // }
-        //     }
-        // });
-        // check_done.addEventListener("change", (e) => {
-        //     if (check_done.checked) {
-        //         check_notDone.checked = false;
-        //         check_all.checked = false;
-        //         // for(let i=0; ll=lis[i]; i++) {
-        //         //     if (ll[0].checked){
-        //         //         ll.style.visibility = 'visible';
-        //         //         ll.style.position = 'relative';
-        //         //     }
-        //         //     else{
-        //         //         ll.style.visibility = 'hidden';
-        //         //         ll.style.position = 'absolute';
-        //         //     }
-        //         // }
-        //     }
-        // });
-        // check_all.addEventListener("change", (e) => {
-        //     if (check_all.checked) {
-        //         check_done.checked = false;
-        //         check_notDone.checked = false;
-        //         // for(let i=0; ll=lis[i]; i++) {
-        //         //     if (ll[0].checked){
-        //         //         ll.style.visibility = 'visible';
-        //         //         ll.style.position = 'relative';
-        //         //     }
-        //         //     else{
-        //         //         ll.style.visibility = 'visible';
-        //         //         ll.style.position = 'relative';
-        //         //     }
-        //         // }
-        //     }
-        // });
         filter.addEventListener('click', event => {
             event.preventDefault();
 
             let currentFilter = event.target.closest('a');
 
-            if (currentFilter){
+            if (currentFilter) {
                 if (currentFilter.getAttribute('id') === 'done')
                     display(doneTasks, notDoneTasks);
 
                 if (currentFilter.getAttribute('id') === 'not-done')
                     display(notDoneTasks, doneTasks);
 
-                if (currentFilter.getAttribute('id') === 'all'){
+                if (currentFilter.getAttribute('id') === 'all') {
                     display(doneTasks, notDoneTasks, true);
                 }
             }
         });
 
-        function display(show, hide, isDisplayAll = false){
-            if (!isDisplayAll){
-                for(item of show){
+        function makeChecked() {
+            if (!check.checked) {
+                check.checked = true;
+                li.id = "liChecked";
+                check.className = "_done"
+            } else {
+                check.checked = false;
+                li.id = null;
+                check.className = "_notDone"
+            }
+        }
+
+        function display(show, hide, isDisplayAll = false) {
+            if (!isDisplayAll) {
+                for (item of show) {
                     let tt = item.closest('li');
                     tt.style.visibility = 'visible';
                     tt.style.position = 'relative';
                 }
-                for(item of hide){
+                for (item of hide) {
                     let tt = item.closest('li');
                     tt.style.visibility = 'hidden';
                     tt.style.position = 'absolute';
                 }
-            }
-            else{
-                for(item of show){
+            } else {
+                for (item of show) {
                     let tt = item.closest('li');
                     tt.style.visibility = 'visible';
                     tt.style.position = 'relative';
                 }
-                for(item of hide){
+                for (item of hide) {
                     let tt = item.closest('li');
                     tt.style.visibility = 'visible';
                     tt.style.position = 'relative';
@@ -154,18 +97,23 @@ function Add() {
             }
         }
     }
-
-
 }
 
-// function hide(el) {
-//     if (!el.hasAttribute('displayOld')) {
-//         el.setAttribute("displayOld", el.style.display)
-//     }
-//     el.style.display = "none"
-// }
-//
-// function show(el) {
-//     let old = el.getAttribute("displayOld");
-//     el.style.display = old || "";
-// }
+function Clear(){
+    const ul = document.getElementById("tasks");
+    if (confirm("Вы уверены что хотите очистить список???")) {
+        if (confirm("Даю последний шанс одуматься! Вы уверены??")) {
+            ul.innerHTML="";
+        } else {
+            return;
+        }
+    } else {
+        return;
+    }
+}
+
+function Info(){
+    alert("Добро пожаловать в мой ToDo-лист!\nВы можете ввести текст в " +
+        "специальное окно и нажать на копку,\nчтобы добавить новый элемент! :D \nТакже информация фильтруется по завершенности! " +
+        "\nПриятного время провождения!");
+}
