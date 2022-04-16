0ac31af4110366781dd805929352b9d48c7130a9
Mosunova <81022626+pnggsn@users.noreply.github.com>
2022-04-16 23:31:40 +0700

    ver 1.0 finished

--- functions.js
+++ functions.js
@@ -1,5 +1,17 @@
 function Add() {
     let inputText = document.getElementById("input").value;
+    const check_notDone = document.getElementById("not-done");
+    check_notDone.checked = false;
+    const check_done = document.getElementById("done");
+    check_done.checked = false;
+    const check_all = document.getElementById("all");
+    check_all.checked = true;
+    //const lis = document.querySelectorAll('li');
+    const doneTasks = document.getElementsByClassName('_done');
+    const notDoneTasks = document.getElementsByClassName('_notDone');
+    const allTasks = document.getElementsByTagName('li');
+    const filter = document.querySelector('.sorting')
+    let item = allTasks;
 
     if (inputText !== "") {
         const ul = document.getElementById("tasks");
@@ -13,6 +25,8 @@ function Add() {
 
         const check = document.createElement("input");
         check.type = "checkbox";
+        check.id = "liCheck";
+        check.className = "_notDone";
 
         li.appendChild(check);
         li.appendChild(btn);
@@ -27,55 +41,131 @@ function Add() {
             if (!check.checked) {
                 check.checked = true;
                 li.id = "liChecked";
+                check.className="_done"
             } else {
                 check.checked = false;
                 li.id = null;
+                check.className="_notDone"
             }
         });
         check.addEventListener('click', (e) => {
             if (!check.checked) {
                 check.checked = true;
                 li.id = "liChecked";
+                check.className="_done"
             } else {
                 check.checked = false;
                 li.id = null;
+                check.className="_notDone"
             }
         });
-    }
 
-    const check_notDone = document.getElementById("not-done");
-    const check_done = document.getElementById("done");
-    const check_all = document.getElementById("all");
+        // check_notDone.addEventListener("change", (e) => {
+        //     if (check_notDone.checked) {
+        //         check_done.checked = false;
+        //         check_all.checked = false;
+        //
+        //         // for(let i=0; ll=lis[i]; i++) {
+        //         //     if (ll[0].checked){
+        //         //         ll.style.visibility = 'hidden';
+        //         //         ll.style.position = 'absolute';
+        //         //     }
+        //         //     else {
+        //         //         ll.style.visibility = 'visible';
+        //         //         ll.style.position = 'relative';
+        //         //     }
+        //         // }
+        //     }
+        // });
+        // check_done.addEventListener("change", (e) => {
+        //     if (check_done.checked) {
+        //         check_notDone.checked = false;
+        //         check_all.checked = false;
+        //         // for(let i=0; ll=lis[i]; i++) {
+        //         //     if (ll[0].checked){
+        //         //         ll.style.visibility = 'visible';
+        //         //         ll.style.position = 'relative';
+        //         //     }
+        //         //     else{
+        //         //         ll.style.visibility = 'hidden';
+        //         //         ll.style.position = 'absolute';
+        //         //     }
+        //         // }
+        //     }
+        // });
+        // check_all.addEventListener("change", (e) => {
+        //     if (check_all.checked) {
+        //         check_done.checked = false;
+        //         check_notDone.checked = false;
+        //         // for(let i=0; ll=lis[i]; i++) {
+        //         //     if (ll[0].checked){
+        //         //         ll.style.visibility = 'visible';
+        //         //         ll.style.position = 'relative';
+        //         //     }
+        //         //     else{
+        //         //         ll.style.visibility = 'visible';
+        //         //         ll.style.position = 'relative';
+        //         //     }
+        //         // }
+        //     }
+        // });
+        filter.addEventListener('click', event => {
+            event.preventDefault();
 
-    check_notDone.addEventListener("change", (e) => {
-        if (check_notDone.checked) {
-            check_done.checked = false;
-            check_all.checked = false;
-            let liList = document.getElementsByTagName('li');
-            for (let i = 0; i < liList.length; i++) {
-                if(liList[i].childNodes[0].nodeValue){
-                    console.log(liList[i].children[0].value);
-                    hide(liList[i]);
+            let currentFilter = event.target.closest('a');
+
+            if (currentFilter){
+                if (currentFilter.getAttribute('id') === 'done')
+                    display(doneTasks, notDoneTasks);
+
+                if (currentFilter.getAttribute('id') === 'not-done')
+                    display(notDoneTasks, doneTasks);
+
+                if (currentFilter.getAttribute('id') === 'all'){
+                    display(doneTasks, notDoneTasks, true);
                 }
             }
-            //console.log(liList[0].children[0]);
-            // for (let li in liList) {
-            //     console.log(li.firstChild);
-            //     // if(li.firstChild.checked){
-            //     //     li.visible = false;
-            //     // }
-            // }
+        });
+
+        function display(show, hide, isDisplayAll = false){
+            if (!isDisplayAll){
+                for(item of show){
+                    let tt = item.closest('li');
+                    tt.style.visibility = 'visible';
+                    tt.style.position = 'relative';
+                }
+                for(item of hide){
+                    let tt = item.closest('li');
+                    tt.style.visibility = 'hidden';
+                    tt.style.position = 'absolute';
+                }
+            }
+            else{
+                for(item of show){
+                    let tt = item.closest('li');
+                    tt.style.visibility = 'visible';
+                    tt.style.position = 'relative';
+                }
+                for(item of hide){
+                    let tt = item.closest('li');
+                    tt.style.visibility = 'visible';
+                    tt.style.position = 'relative';
+                }
+            }
         }
-    });
-}
-function hide(el) {
-    if (!el.hasAttribute('displayOld')) {
-        el.setAttribute("displayOld", el.style.display)
     }
-    el.style.display = "none"
+
+
 }
 
-function show(el) {
-    let old = el.getAttribute("displayOld");
-    el.style.display = old || "";
-}
+// function hide(el) {
+//     if (!el.hasAttribute('displayOld')) {
+//         el.setAttribute("displayOld", el.style.display)
+//     }
+//     el.style.display = "none"
+// }
+//
+// function show(el) {
+//     let old = el.getAttribute("displayOld");
+//     el.style.display = old || "";
+// }
